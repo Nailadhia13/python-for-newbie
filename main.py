375d0627ee4ec5a180c495e340b9bf16d0bc4906
Nailadhia13 <nailadhia07@gmail.com>
2026-01-09 21:28:46 +0700

    pemula weh

---
+++ main.py
@@ -0,0 +1,18 @@
+# Halo semua! Aku Nailadhia Martafani
+# aku mahasiswa ilmu komputer di UNILA
+# aku baru saja belajar new language, yaitu python setelah sebelumnya belajar c++
+# untuk kalian yang juga baru belajar, boleh pahami kode-kode di bawah ini ya
+# awal mula dibuaut tanggal 9/1/26
+
+print("====================================")
+print("             PYTHON DASAR           ")
+print("====================================")
+
+# tipe data
+print("hello world")  # akan menampilkan output dalam()
+print("hello " * 3)  # akan mencetak hello sebanyak 3 kali (looping)
+# akan mencetak  kalimat yang sama seperti yang di line 1, "" dan '' sama fungsinya
+print('hello world')
+# akan mencetak kalimat yg sama seperti line sebelumnya
+print('Hello ' + 'world')
+# print("Hello" * "world") ,, ini akan error karena perkalian tidak bisa digunakan dalam tipe data string
